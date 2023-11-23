2791262008642fc3a4f46ed58e6cd1619ded042a
Yasmin <yasminehelmy2001@gmail.com>
2023-11-23 09:35:54 +0200

    commited

---
+++ AdminLinux_Task1/BashScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/yasmin
+mkdir myDirectory
+cd ./myDirectory
+mkdir secondDirectory
+cd ./secondDirectory
+touch myNotePaper
+cp myNotePaper /home/yasmin/myDirectory
+cd ../
+mv myNotePaper myOldNotePaper
